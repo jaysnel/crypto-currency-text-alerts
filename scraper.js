41c79c26c8a343b5273a0b90c26b25c6b06faaad
Jaylan Snelson <jsnelson@bizjournals.com>
2020-06-17 10:27:13 -0400

    getting data and sending to a json file for now, getting ready to set up twilio

--- scraper.js
+++ scraper.js
@@ -6,26 +6,38 @@ axios.get('https://coinmarketcap.com/').then((res) => {
 
     const $ = cheerio.load(res.data);
     
-    const mainCoinSection = $('.cmc-table__table-wrapper-outer tbody tr td');
-    const sectionTitle = $('.sc-1yv6u5n-0 .cmc-table__table-wrapper-outer thead').last().find('tr th');
-    const bodyContent = $('.sc-1yv6u5n-0 .cmc-table__table-wrapper-outer tbody').last().find('tr');
+    const coinTitles = $('.sc-1yv6u5n-0 .cmc-table__table-wrapper-outer thead').last().find('tr th');
+    const coinInformation = $('.sc-1yv6u5n-0 .cmc-table__table-wrapper-outer tbody').last().find('tr');
 
     // Looping through column titles
-    let myArray = [];
-    
-    for(let i = 0; i <= sectionTitle.length - 2; i++) {
-        const title = sectionTitle.eq([i]).html();
-        myArray.push(title);
-        console.log(title);
+    let coinTitleArray = []; //controls how many columns you show from the homepage
+    let topCoins = []; //coins shown
+
+    for(let i = 0; i <= coinTitles.length - 2; i++) { //populates coinTitleArray with the columns you want to pull
+        const title = coinTitles.eq([i]).html();
+        coinTitleArray.push(title);
     }
 
     // Looping through column content for each coin
-    for(let i = 0; i < bodyContent.length; i++) {
-        const content = bodyContent.find('td').eq([i]).text();
-        console.log(content);
-    }
+    //using number to get the top 10. If you want to get all, change to coinInformation.length
+    let finalObject = {};
+    for(let i = 0; i < 5; i++) {
+        const content = coinInformation.eq([i]).find('td');
+        let finalObject = {};
 
-    fs.writeFile('coin-titles.txt', myArray.join().replace(/,/g, ' '), function(err) {if (err) console.log(err);});
+        coinTitleArray.forEach((el, j) => { finalObject[el] = content.eq([j]).text();})
+        topCoins.push(finalObject);
+    }
+    
+    // adding data to a .json file
+    function prettyData(arr) {
+        fs.writeFile('coin-titles.json', ' ', function(err) {if (err) console.log(err);});
+        arr.forEach(el => {
+            let stringify = JSON.stringify(el)
+            fs.appendFile('coin-titles.json', stringify + '\n', function(err) {if (err) console.log(err);});
+        })
+    }
+    prettyData(topCoins);
 })
 .catch(err => {
     console.log(err)

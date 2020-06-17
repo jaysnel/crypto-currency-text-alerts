c4fcf28a951c4a26871f8935389ee454318f181f
Jaylan Snelson <jsnelson@bizjournals.com>
2020-06-17 09:12:31 -0400

    looping through coin sections

--- scraper.js
+++ scraper.js
@@ -7,14 +7,22 @@ axios.get('https://coinmarketcap.com/').then((res) => {
     const $ = cheerio.load(res.data);
     
     const mainCoinSection = $('.cmc-table__table-wrapper-outer tbody tr td');
-    const sectionTitle = $('.sc-1yv6u5n-0 .cmc-table__table-wrapper-outer').last().find('tr th');
+    const sectionTitle = $('.sc-1yv6u5n-0 .cmc-table__table-wrapper-outer thead').last().find('tr th');
+    const bodyContent = $('.sc-1yv6u5n-0 .cmc-table__table-wrapper-outer tbody').last().find('tr');
 
+    // Looping through column titles
     let myArray = [];
-
+    
     for(let i = 0; i <= sectionTitle.length - 2; i++) {
         const title = sectionTitle.eq([i]).html();
         myArray.push(title);
         console.log(title);
+    }
+
+    // Looping through column content for each coin
+    for(let i = 0; i < bodyContent.length; i++) {
+        const content = bodyContent.find('td').eq([i]).text();
+        console.log(content);
     }
 
     fs.writeFile('coin-titles.txt', myArray.join().replace(/,/g, ' '), function(err) {if (err) console.log(err);});

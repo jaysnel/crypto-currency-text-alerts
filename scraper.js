2d67892e323000099ed717a8b4f19913241b6a3c
Jaylan Snelson <jsnelson@bizjournals.com>
2020-06-17 10:35:32 -0400

    needed synchronous call to write items in order

--- scraper.js
+++ scraper.js
@@ -3,15 +3,13 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 
 axios.get('https://coinmarketcap.com/').then((res) => {
-
     const $ = cheerio.load(res.data);
-    
     const coinTitles = $('.sc-1yv6u5n-0 .cmc-table__table-wrapper-outer thead').last().find('tr th');
     const coinInformation = $('.sc-1yv6u5n-0 .cmc-table__table-wrapper-outer tbody').last().find('tr');
 
     // Looping through column titles
     let coinTitleArray = []; //controls how many columns you show from the homepage
-    let topCoins = []; //coins shown
+    let finalShownCoins = [];
 
     for(let i = 0; i <= coinTitles.length - 2; i++) { //populates coinTitleArray with the columns you want to pull
         const title = coinTitles.eq([i]).html();
@@ -22,22 +20,22 @@ axios.get('https://coinmarketcap.com/').then((res) => {
     //using number to get the top 10. If you want to get all, change to coinInformation.length
     let finalObject = {};
     for(let i = 0; i < 5; i++) {
-        const content = coinInformation.eq([i]).find('td');
+        const coinContent = coinInformation.eq([i]).find('td');
         let finalObject = {};
 
-        coinTitleArray.forEach((el, j) => { finalObject[el] = content.eq([j]).text();})
-        topCoins.push(finalObject);
+        coinTitleArray.forEach((el, j) => { finalObject[el] = coinContent.eq([j]).text();})
+        finalShownCoins.push(finalObject);
     }
     
     // adding data to a .json file
     function prettyData(arr) {
-        fs.writeFile('coin-titles.json', ' ', function(err) {if (err) console.log(err);});
+        fs.writeFile('coin-titles.json', '', function(err) {if (err) console.log(err);});
         arr.forEach(el => {
-            let stringify = JSON.stringify(el)
-            fs.appendFile('coin-titles.json', stringify + '\n', function(err) {if (err) console.log(err);});
+            let stringify = JSON.stringify(el);
+            fs.appendFileSync('coin-titles.json', stringify + '\n', function(err) {if (err) console.log(err);});
         })
     }
-    prettyData(topCoins);
+    prettyData(finalShownCoins);
 })
 .catch(err => {
     console.log(err)

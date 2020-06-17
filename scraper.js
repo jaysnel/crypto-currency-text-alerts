c35c940ba57a41dca83c66206e41800624f9fd6f
Jaylan Snelson <jaylan210@gmail.com>
2020-06-16 22:59:45 -0400

    getting titles for each section

--- scraper.js
+++ scraper.js
@@ -1,24 +1,24 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const fs = require('fs');
 
 axios.get('https://coinmarketcap.com/').then((res) => {
 
     const $ = cheerio.load(res.data);
     
     const mainCoinSection = $('.cmc-table__table-wrapper-outer tbody tr td');
-    const mainCoinSection2 = $('.cmc-table__table-wrapper-outer tbody tr');
+    const sectionTitle = $('.sc-1yv6u5n-0 .cmc-table__table-wrapper-outer').last().find('tr th');
 
-    console.log(mainCoinSection.html());
+    let myArray = [];
 
-    // const pageHeader = $('.page-header');
-    // const output = pageHeader.find('h1').text();
+    for(let i = 0; i <= sectionTitle.length - 2; i++) {
+        const title = sectionTitle.eq([i]).html();
+        myArray.push(title);
+        console.log(title);
+    }
 
-    console.log(mainCoinSection2.length);
-
-    $(mainCoinSection2).each((i, el) => {
-        el.find('td').each(function() {
-            console.log(this.text())
-          });   
-    })
-    
+    fs.writeFile('coin-titles.txt', myArray.join().replace(/,/g, ' '), function(err) {if (err) console.log(err);});
+})
+.catch(err => {
+    console.log(err)
 })

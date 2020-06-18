b4a6f16c08b84cb4c4439a9b22a6c54fe3498352
unknown <jaylan210@gmail.com>
2020-06-17 23:27:41 -0400

    updating variable names to make more sense

--- scraper.js
+++ scraper.js
@@ -35,39 +35,31 @@ function sendTextMessage() {
 //////////////////////////////////////
 axios.get('https://coinmarketcap.com/').then((res) => {
     const $ = cheerio.load(res.data);
-    const coinTitles = $('.sc-1yv6u5n-0 .cmc-table__table-wrapper-outer thead').last().find('tr th');
-    const coinInformation = $('.sc-1yv6u5n-0 .cmc-table__table-wrapper-outer tbody').last().find('tr');
+    const cryptoCategoryTitle = $('.sc-1yv6u5n-0 .cmc-table__table-wrapper-outer thead').last().find('tr th');
+    const cryptoCurrencyInformation = $('.sc-1yv6u5n-0 .cmc-table__table-wrapper-outer tbody').last().find('tr');
 
     // Looping through column titles
-    let coinTitleArray = []; //controls how many columns you show from the homepage
+    let cryptoCategoryTitleArray = []; //controls how many catergories you want to show from the homepage
     let finalShownCoins = [];
 
-    for(let i = 0; i <= coinTitles.length - 2; i++) { //populates coinTitleArray with the columns you want to pull
-        const title = coinTitles.eq([i]).html();
-        coinTitleArray.push(title);
+    for(let i = 0; i <= cryptoCategoryTitle.length - 2; i++) { //populates cryptoCategoryTitleArray with the catergories you want to pull
+        const title = cryptoCategoryTitle.eq([i]).html();
+        cryptoCategoryTitleArray.push(title);
     }
 
     //using topCoins and for loop to get the top n crypto currencies.
     let topCoins = 20;
     for(let i = 0; i < topCoins; i++) {
-        const coinContent = coinInformation.eq([i]).find('td');
+        const coinContent = cryptoCurrencyInformation.eq([i]).find('td');
         let coinsToShow = {};
 
-        coinTitleArray.forEach((el, j) => { coinsToShow[el] = coinContent.eq([j]).text();})
+        cryptoCategoryTitleArray.forEach((el, j) => { coinsToShow[el] = coinContent.eq([j]).text();})
         finalShownCoins.push(coinsToShow);
     }
 
     //creating/updating file to send text message
     function setUpData() {
         fs.writeFile(cryptoFileName, '', function(err) {if (err) console.log(err);});
-        
-        // Titles are here
-        // coinTitleArray.forEach(el => {
-        //     if(el == "Rank") fs.appendFileSync('coin-titles.txt', el + ' ', function(err) {if (err) console.log(err);});
-        //     if(el == "Name") fs.appendFileSync('coin-titles.txt', el + ' ', function(err) {if (err) console.log(err);});
-        //     if(el == "Price") fs.appendFileSync('coin-titles.txt', el + ' ', function(err) {if (err) console.log(err);});
-        //     if(el == "Change (24h)") fs.appendFileSync('coin-titles.txt', el + ' ', function(err) {if (err) console.log(err);});
-        // })
 
         finalShownCoins.forEach(el => {
             fs.appendFileSync(cryptoFileName, '\n' + el.Rank + ' ' + el.Name + ' ' + el.Price + ' ' + el['Change (24h)'] + ',' + '\n', function(err) {if (err) console.log(err);});

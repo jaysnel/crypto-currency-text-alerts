7146c0249db89b4f012f7120c8df46f09e96b19f
unknown <jaylan210@gmail.com>
2020-06-17 23:15:19 -0400

    full twilio set up, code cleanup, all varaibles set

--- scraper.js
+++ scraper.js
@@ -1,19 +1,38 @@
+//////////////////////////////////////
+//      Variable Set Up
+//////////////////////////////////////
+const dotenv = require('dotenv');
+dotenv.config();
 const axios = require('axios');
 const cheerio = require('cheerio');
 const fs = require('fs');
-const accountSid = 'AC23accf08ab9a97b00a23d577bf21a7d3';
-const authToken = '24ed6f367c4887e3e99bef8d12b0da33';
+const twilioNumber = process.env.TWILIOPHONENUMBER;
+const phoneNumber = process.env.PHONENUMBER;
+const accountSid = process.env.ACCOUNTSID;
+const authToken = process.env.AUTHTOKEN;
 const client = require('twilio')(accountSid, authToken);
+const cryptoFileName = './top-crypto-coins.txt';
+require.extensions['.txt'] = function (module, filename) {
+    module.exports = fs.readFileSync(filename, 'utf8');
+};
 
-client.messages
-  .create({
-     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-     from: '+17043248153',
-     to: '+18106241057'
-   })
-  .then(message => console.log(message));
+//////////////////////////////////////
+//      Twilio Send Text
+//////////////////////////////////////
+function sendTextMessage() {
+    const msg = require(cryptoFileName);
+    client.messages
+        .create({
+            body: msg,
+            from: twilioNumber,
+            to: phoneNumber
+        })
+        .then(message => console.log(message));
+}
 
-
+//////////////////////////////////////
+//      Web Scraper
+//////////////////////////////////////
 axios.get('https://coinmarketcap.com/').then((res) => {
     const $ = cheerio.load(res.data);
     const coinTitles = $('.sc-1yv6u5n-0 .cmc-table__table-wrapper-outer thead').last().find('tr th');
@@ -30,31 +49,32 @@ axios.get('https://coinmarketcap.com/').then((res) => {
 
     // Looping through column content for each coin
     //using number to get the top 10. If you want to get all, change to coinInformation.length
-    let finalObject = {};
-    let topCoinsIWantToSee = 20;
-    for(let i = 0; i < topCoinsIWantToSee; i++) {
+    let topCoins = 20;
+    for(let i = 0; i < topCoins; i++) {
         const coinContent = coinInformation.eq([i]).find('td');
-        let finalObject = {};
+        let coinsToShow = {};
 
-        coinTitleArray.forEach((el, j) => { finalObject[el] = coinContent.eq([j]).text();})
-        finalShownCoins.push(finalObject);
+        coinTitleArray.forEach((el, j) => { coinsToShow[el] = coinContent.eq([j]).text();})
+        finalShownCoins.push(coinsToShow);
     }
 
-    function saveData() {
-        fs.writeFile('coin-titles.txt', '', function(err) {if (err) console.log(err);});
+    function setUpData() {
+        fs.writeFile(cryptoFileName, '', function(err) {if (err) console.log(err);});
         
-        coinTitleArray.forEach(el => {
-            if(el == "Rank") fs.appendFileSync('coin-titles.txt', el + ' ', function(err) {if (err) console.log(err);});
-            if(el == "Name") fs.appendFileSync('coin-titles.txt', el + ' ', function(err) {if (err) console.log(err);});
-            if(el == "Price") fs.appendFileSync('coin-titles.txt', el + ' ', function(err) {if (err) console.log(err);});
-            if(el == "Change (24h)") fs.appendFileSync('coin-titles.txt', el + ' ', function(err) {if (err) console.log(err);});
-        })
+        // Titles are here
+        // coinTitleArray.forEach(el => {
+        //     if(el == "Rank") fs.appendFileSync('coin-titles.txt', el + ' ', function(err) {if (err) console.log(err);});
+        //     if(el == "Name") fs.appendFileSync('coin-titles.txt', el + ' ', function(err) {if (err) console.log(err);});
+        //     if(el == "Price") fs.appendFileSync('coin-titles.txt', el + ' ', function(err) {if (err) console.log(err);});
+        //     if(el == "Change (24h)") fs.appendFileSync('coin-titles.txt', el + ' ', function(err) {if (err) console.log(err);});
+        // })
 
         finalShownCoins.forEach(el => {
-            fs.appendFileSync('coin-titles.txt', '\n' + el.Rank + ' ' + el.Name + ' ' + el.Price + ' ' + el['Change (24h)'] + ',', function(err) {if (err) console.log(err);});
+            fs.appendFileSync(cryptoFileName, '\n' + el.Rank + ' ' + el.Name + ' ' + el.Price + ' ' + el['Change (24h)'] + ',' + '\n', function(err) {if (err) console.log(err);});
         })
+        sendTextMessage();
     }
-    saveData();
+    setUpData();
     })
     .catch(err => {
         console.log(err)
